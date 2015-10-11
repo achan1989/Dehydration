55bc347f24aa31ce204b1019e31ac879f9acb3b1
achan1989 <achan1989@gmail.com>
2015-10-11 11:03:23 +0100

    Create a variant of JobGiver_ExitMapWalkRandom that can be interrupted.

---
+++ Source/dehydration/src/jobs/JobGiver_ExitMapWalkRandomInterruptable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace achan1989.dehydration
+{
+    public class JobGiver_ExitMapWalkRandomInterruptable : JobGiver_ExitMapWalkRandom
+    {
+        protected override Job TryGiveTerminalJob(Pawn pawn)
+        {
+            // Exit the map, but stop doing that if there's something more important to do.
+            var job = base.TryGiveTerminalJob(pawn);
+            job.expiryInterval = 900;
+            job.checkOverrideOnExpire = true;
+
+            return job;
+        }
+    }
+}
