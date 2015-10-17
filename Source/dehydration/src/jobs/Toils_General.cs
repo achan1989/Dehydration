ebc0f51d46599636ac0f5b54d1059677ee0b6077
achan1989 <achan1989@gmail.com>
2015-10-17 17:30:30 +0100

    Add Toils_General to cover things that base is lacking.

---
+++ Source/dehydration/src/jobs/Toils_General.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace achan1989.dehydration
+{
+    public static class Toils_General
+    {
+        public static Toil CarryThingFromInventory(Thing thing)
+        {
+            var toil = new Toil();
+            toil.defaultCompleteMode = ToilCompleteMode.Instant;
+            toil.initAction = delegate
+            {
+                Pawn actor = toil.actor;
+                if (actor.inventory != null && actor.inventory.container.Contains(thing))
+                {
+                    actor.inventory.container.TransferToContainer(thing, actor.carrier.container, 1);
+                }
+                else
+                {
+                    Log.Error(string.Format("{0} can't transfer {1} from inventory to hands.", actor, thing));
+                    actor.jobs.EndCurrentJob(JobCondition.Errored);
+                    return;
+                }
+            };
+            return toil;
+        }
+
+        public static Toil FaceThing(TargetIndex thingInd)
+        {
+            var toil = new Toil();
+            toil.defaultCompleteMode = ToilCompleteMode.Instant;
+            toil.initAction = delegate
+            {
+                Pawn actor = toil.actor;
+                actor.drawer.rotator.FaceCell(actor.jobs.curJob.GetTarget(thingInd).Cell);
+            };
+            return toil;
+        }
+    }
+}
