91eed2df430078b54405e31c204ab6e9019dff45
achan1989 <achan1989@gmail.com>
2015-10-25 14:44:11 +0000

    Can't place wells on Ice terrain.

---
+++ Source/dehydration/src/PlaceWorker_ValidForWell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace achan1989.dehydration
+{
+    public class PlaceWorker_ValidForWell : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot)
+        {
+            foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size))
+            {
+                var terrain = Find.TerrainGrid.TerrainAt(current);
+                if (terrain.defName.Equals("Ice")) { return "Cannot be placed on ice."; }
+            }
+            return true;
+        }
+    }
+}
